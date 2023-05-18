71e37a558b754a6abd8b3b037ca77cc0bf471eba
Tomasz <>
2023-05-18 13:53:49 +0200

    Created routing

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hello there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH); 
+
+Routing::get('index', 'DefaultController');
+Routing::get('projects', 'DefaultController');
+
+Routing::run($path);
